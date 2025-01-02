cb47a395159887dd65168d948b8d37e9f3dccec0
EliasDeHondt <Elias.dehondt@outlook.com>
2025-01-02 21:40:54 +0100

    [TASK]: Building a Modern Search Page: From Scratch with HTML, CSS, and JavaScript #7 Starting point.

---
+++ App/Frontend/js/search.js
@@ -0,0 +1,10 @@
+/**********************************/
+/* @since 01/01/2025              */
+/* @author K10s Open Source Team  */
+/**********************************/
+
+// Load external content
+document.addEventListener('DOMContentLoaded', function() {
+    loadExternalContent("navigation-bar", "/App/Frontend/includes/navigation-bar.html");
+    loadExternalContent("context-menu", "/App/Frontend/includes/context-menu.html");
+});
