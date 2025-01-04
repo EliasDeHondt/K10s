6a625e34c773e42c0a9bbfa84be540dd8dc2daa6
EliasDeHondt <Elias.dehondt@outlook.com>
2025-01-04 19:57:02 +0100

    Add footer section and update cursor styles; refactor login and dashboard scripts to include footer and context menu

--- App/Frontend/js/dashboard.js
+++ App/Frontend/js/dashboard.js
@@ -5,6 +5,22 @@
 
 // Load external content
 document.addEventListener('DOMContentLoaded', function() {
-    loadExternalContent("navigation-bar", "/App/Frontend/includes/navigation-bar.html");
+    loadExternalContent("nav", "/App/Frontend/includes/nav.html");
+    loadExternalContent("footer", "/App/Frontend/includes/footer.html");
     loadExternalContent("context-menu", "/App/Frontend/includes/context-menu.html");
+});
+
+// Fullscreen button
+document.getElementById('dashboard-fullscreen-button').addEventListener('click', () => {
+    const dashboardMain = document.getElementById('dashboard-main');
+    const dashboardTitle = document.getElementById('dashboard-title');
+    if (!dashboardMain.classList.contains('fullscreen')) {
+        dashboardMain.classList.add('fullscreen');
+        dashboardMain.style.gridArea = '1 / 1 / -1 / -1';
+        dashboardTitle.style.display = 'block';
+    } else {
+        dashboardMain.classList.remove('fullscreen');
+        dashboardMain.style.gridArea = '';
+        dashboardTitle.style.display = 'none';
+    }
 });
--- App/Frontend/js/search.js
+++ App/Frontend/js/search.js
@@ -5,6 +5,7 @@
 
 // Load external content
 document.addEventListener('DOMContentLoaded', function() {
-    loadExternalContent("navigation-bar", "/App/Frontend/includes/navigation-bar.html");
+    loadExternalContent("nav", "/App/Frontend/includes/nav.html");
+    loadExternalContent("footer", "/App/Frontend/includes/footer.html");
     loadExternalContent("context-menu", "/App/Frontend/includes/context-menu.html");
 });
