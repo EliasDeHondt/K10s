860cc8b00924bab4f0a6fc0840f90bf126c959e9
EliasDeHondt <Elias.dehondt@outlook.com>
2025-01-18 10:50:13 +0100

    Implement fullscreen toggle functionality for the dashboard and update HTML structure #11

--- App/Frontend/js/dashboard.js
+++ App/Frontend/js/dashboard.js
@@ -12,13 +12,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Fullscreen button
 document.getElementById('dashboard-fullscreen-button').addEventListener('click', () => {
+    const dashboardHtml = document.documentElement;
     const dashboardMain = document.getElementById('dashboard-main');
     const dashboardTitle = document.getElementById('dashboard-title');
-    if (!dashboardMain.classList.contains('fullscreen')) {
+
+    if (!document.fullscreenElement) {
+        if (dashboardHtml.requestFullscreen) dashboardHtml.requestFullscreen();
+        else if (dashboardHtml.mozRequestFullScreen) dashboardHtml.mozRequestFullScreen();
+        else if (dashboardHtml.webkitRequestFullscreen) dashboardHtml.webkitRequestFullscreen();
+        else if (dashboardHtml.msRequestFullscreen) dashboardHtml.msRequestFullscreen();
+
         dashboardMain.classList.add('fullscreen');
         dashboardMain.style.gridArea = '1 / 1 / -1 / -1';
         dashboardTitle.style.display = 'block';
     } else {
+        if (document.exitFullscreen) document.exitFullscreen();
+        else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+        else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+        else if (document.msExitFullscreen) document.msExitFullscreen();
+
         dashboardMain.classList.remove('fullscreen');
         dashboardMain.style.gridArea = '';
         dashboardTitle.style.display = 'none';
