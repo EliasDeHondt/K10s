30de93bed859ec2929645b2d90bed821e98f985d
Vincent Verboven <vincent.verboven85@gmail.com>
2025-02-03 15:00:23 +0100

    #6 Added auth middleware

---
+++ App/Backend/cmd/auth/AuthMiddleware.go
@@ -0,0 +1,50 @@
+package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"net/http"
+	"time"
+)
+
+var jwtSecret = []byte("L7Naq/2kj9tAYs2Aap47w5Pzo7q/HhtsVFB/zgGnLHg=") //[]byte(os.Getenv("JWT_SECRET"))
+
+type JwtClaims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		jwtTokenString, err := ctx.Cookie("jwt")
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			return
+		}
+
+		token, err := jwt.Parse(jwtTokenString, func(token *jwt.Token) (interface{}, error) {
+			return jwtSecret, nil
+		})
+
+		if err != nil || !token.Valid {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			return
+		}
+
+		ctx.Next()
+	}
+}
+
+func GenerateToken(username string) (string, error) {
+	claims := JwtClaims{
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtSecret)
+}
