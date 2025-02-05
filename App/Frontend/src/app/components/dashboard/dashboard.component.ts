e885547fa30f18dd77b4e765364fdf9b327c70ea
VeraWise <vera.wise@student.kdg.be>
2025-02-05 14:25:23 +0100

    angular rework: it runs

--- App/Frontend/src/app/components/dashboard/dashboard.component.ts
+++ App/Frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-    selector: 'app-dashboard',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-    title = 'K10s';
+  title = 'K10s';
 }
