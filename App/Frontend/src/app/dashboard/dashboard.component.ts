ba24e37a1e42cfbcdaff5e1851a6244caf8a5434
VeraWise <vera.wise@student.kdg.be>
2025-02-06 20:51:12 +0100

    angular rework: import component in component

--- App/Frontend/src/app/dashboard/dashboard.component.ts
+++ App/Frontend/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NavComponent} from '../nav/nav.component';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  imports: [NavComponent],
+  standalone: true
 })
 export class DashboardComponent {
   title = 'K10s';
--- App/Frontend/src/app/nav/nav.component.ts
+++ App/Frontend/src/app/nav/nav.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-nav',
-  imports: [],
   templateUrl: './nav.component.html',
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
   styleUrl: './nav.component.css'
 })
-export class NavComponent {
-
-}
+export class NavComponent {}
