da14b2032babfcbcff4c5f11750349b4a9a00ce4
Vincent Verboven <vincent.verboven85@gmail.com>
2025-02-07 16:51:31 +0100

    Removed clustername from node and added pods amount

--- App/Backend/cmd/kubernetes/Types.go
+++ App/Backend/cmd/kubernetes/Types.go
@@ -1,30 +1,33 @@
 package kubernetes
 
 import (
+	"context"
 	"fmt"
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"time"
 )
 
 type Node struct {
-	ClusterName string
-	Name        string
-	Status      string
-	IP          string
-	Role        string
-	Version     string
-	NodeAge     string
+	Name       string
+	Status     string
+	Role       string
+	Version    string
+	PodsAmount int
+	NodeAge    string
+	IP         string
 }
 
-func NewNode(node v1.Node) Node {
+func NewNode(node v1.Node, clientset *kubernetes.Clientset) Node {
 	return Node{
-		ClusterName: node.Namespace,
-		Name:        node.Name,
-		Status:      isNodeOnline(&node),
-		IP:          node.Status.Addresses[0].Address,
-		Role:        node.Labels["kubernetes.io/role"],
-		Version:     node.ObjectMeta.Labels["version"],
-		NodeAge:     calculateNodeAge(&node),
+		Name:       node.Name,
+		Status:     isNodeOnline(&node),
+		Role:       node.Labels["kubernetes.io/role"],
+		Version:    node.ObjectMeta.Labels["version"],
+		PodsAmount: getPodsInNode(node.Name, clientset),
+		NodeAge:    calculateNodeAge(&node),
+		IP:         node.Status.Addresses[0].Address,
 	}
 }
 
@@ -32,6 +35,19 @@ func calculateNodeAge(node *v1.Node) string {
 	age := time.Since(node.CreationTimestamp.Time)
 
 	return fmt.Sprintf("%d:%d:%d", int(age.Hours()/24), int(age.Hours())%24, int(age.Minutes())%60)
+}
+
+func getPodsInNode(nodeName string, clientset *kubernetes.Clientset) int {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	pods, err := clientset.CoreV1().Pods("").List(ctx, metav1.ListOptions{
+		FieldSelector: "spec.nodeName=" + nodeName,
+	})
+	if err != nil {
+		return 0
+	}
+
+	return len(pods.Items)
 }
 
 func isNodeOnline(node *v1.Node) string {
