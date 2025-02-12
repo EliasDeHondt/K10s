c6ad86f84b2476fc43839a21e84d4c9f998b8f77
VeraWise <vera.wise@student.kdg.be>
2025-02-12 14:20:01 +0100

    working hmtl canvas nodes

--- App/Frontend/src/app/app.routes.ts
+++ App/Frontend/src/app/app.routes.ts
@@ -7,10 +7,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SearchComponent } from './search/search.component';
+import {SpiderWebComponent} from "./spider-web/spider-web.component";
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent, data: { title: 'K10s - Login' } },
     { path: 'dashboard', component: DashboardComponent, data: { title: 'K10s - Dashboard' } },
-    { path: 'search', component: SearchComponent, data: { title: 'K10s - Search' } }
+    { path: 'search', component: SearchComponent, data: { title: 'K10s - Search' } },
+    { path: 'asdf', component: SpiderWebComponent, data: { title: 'K10s - Search' } }
 ];
