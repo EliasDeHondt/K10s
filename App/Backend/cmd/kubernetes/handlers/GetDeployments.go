aac17bfac5e19b291ffa9b891b72e59fe46784de
Vincent Verboven <vincent.verboven85@gmail.com>
2025-02-14 13:18:22 +0100

    Added pagination to deployments + performance increase

--- App/Backend/cmd/kubernetes/handlers/GetDeployments.go
+++ App/Backend/cmd/kubernetes/handlers/GetDeployments.go
@@ -4,22 +4,24 @@ import (
 	"context"
 	"github.com/eliasdehondt/K10s/App/Backend/cmd/kubernetes"
 	"github.com/gin-gonic/gin"
+	v1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"net/http"
+	"sync"
 	"time"
 )
 
 func GetDeploymentsHandler(ctx *gin.Context) {
-
 	namespace, ok := ctx.GetQuery("namespace")
+	pageSize, pageToken := GetPageSizeAndPageToken(ctx)
 
-	var deploymentList *[]kubernetes.Deployment
+	var deploymentList *PaginatedResponse[[]kubernetes.Deployment]
 	var err error
 
 	if ok {
-		deploymentList, err = GetDeployments(c, namespace)
+		deploymentList, err = GetDeployments(c, namespace, pageSize, pageToken)
 	} else {
-		deploymentList, err = GetDeployments(c, "")
+		deploymentList, err = GetDeployments(c, "", pageSize, pageToken)
 	}
 
 	if err != nil {
@@ -29,11 +31,14 @@ func GetDeploymentsHandler(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, deploymentList)
 }
 
-func GetDeployments(c *kubernetes.IClient, namespace string) (*[]kubernetes.Deployment, error) {
+func GetDeployments(c *kubernetes.IClient, namespace string, pageSize int, pageToken string) (*PaginatedResponse[[]kubernetes.Deployment], error) {
 	ct, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	list, err := (*c).GetDeployments(namespace).List(ct, metav1.ListOptions{})
+	list, err := (*c).GetDeployments(namespace).List(ct, metav1.ListOptions{
+		Limit:    int64(pageSize),
+		Continue: pageToken,
+	})
 	if err != nil {
 		return nil, err
 	}
@@ -43,5 +48,30 @@ func GetDeployments(c *kubernetes.IClient, namespace string) (*[]kubernetes.Depl
 	for i, deployment := range list.Items {
 		deploymentList[i] = kubernetes.NewDeployment(deployment)
 	}
-	return &deploymentList, nil
+	return &PaginatedResponse[[]kubernetes.Deployment]{
+		Response:  transformDeployments(&list.Items),
+		PageToken: list.Continue,
+	}, nil
+}
+
+func transformDeployments(list *[]v1.Deployment) []kubernetes.Deployment {
+	var deploymentList = make([]kubernetes.Deployment, len(*list))
+
+	var wg sync.WaitGroup
+	concurrency := 20
+	semaphore := make(chan struct{}, concurrency)
+
+	for i, deployment := range *list {
+		wg.Add(1)
+		semaphore <- struct{}{}
+
+		go func(i int, node v1.Deployment) {
+			defer wg.Done()
+			deploymentList[i] = kubernetes.NewDeployment(deployment)
+			<-semaphore
+		}(i, deployment)
+	}
+
+	wg.Wait()
+	return deploymentList
 }
