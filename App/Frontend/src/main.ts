e4cad5b4b3b9abee20f8c74b84f91a4748ade9da
EliasDeHondt <Elias.dehondt@outlook.com>
2025-02-24 12:00:34 +0100

    Update Docker configurations and environment settings for frontend and backend services

--- App/Frontend/src/main.ts
+++ App/Frontend/src/main.ts
@@ -9,6 +9,7 @@ import { appConfig } from './app/app.config';
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { HttpClient } from "@angular/common/http";
 import { TranslateLoader } from "@ngx-translate/core";
+import { environment } from './environments/environment';
 
 export function HttpLoaderFactory(http: HttpClient): TranslateLoader {
     return new TranslateHttpLoader(http, 'assets/i18n/', '.json');
@@ -50,3 +51,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const savedTheme: string | null = localStorage.getItem('theme') || 'light';
     document.documentElement.setAttribute('data-theme', savedTheme);
 });
+
+if (environment.BASE_URL === 'http://localhost:8080') console.log('You are running in development mode');
+if (environment.BASE_URL === '') environment.BASE_URL = `${window.location.origin}/api`;
